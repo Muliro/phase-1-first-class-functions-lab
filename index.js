c942f1c495342927c103d680799747be979ba92e
michael muliro <michael.muliro@student.moringaschool.com>
2022-06-10 15:38:56 +0300

    commit tree 38c1da8e2b3996959d530d65899b8698ff0f42aa

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+    return array.slice(0, 2);
+}
+
+
+
+const returnLastTwoDrivers = function(array){
+    return array.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare){
+    return function(){
+        return fare * 5;
+    }
+}
+
+function fareDoubler(fare){
+    return fare * 2;
+}
+
+function fareTripler(fare){
+    return fare * 3;
+}
+
+function selectDifferentDrivers(driverArray, selector){
+    return selector(driverArray)
+}
